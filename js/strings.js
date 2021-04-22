babd2083e1ac2c6cd7a71f8b4f78f806ac795f5d
jenkpl <jenkpl@gmail.com>
2021-04-22 11:05:51 -0700

    Assignment 10 initial commit

---
+++ js/strings.js
@@ -0,0 +1,27 @@
+//STEP 1:Create an application that prompts the user for their name. Then, find the length of characters in the person’s name. Use the alert method to display the result.
+// let yName = prompt('WELCOME TO THE NAME-LENGTH-O-METER!\nPlease enter your name:');
+// let howLong = yName.length;
+// alert(`RESULTS ARE IN: Your name is: ${howLong} characters long!`);
+
+//STEP 2:Create an application that prompts the user for their name. Then, prompt the user for a numeric value so that they can find the letter in the string based on the number they specify. Use the alert method to display the result.
+let yName  = prompt('WELCOME TO THE NAME-CHARACTER-FINDER!\nPlease enter your name:');
+let max = yName.length;
+let num    = parseInt(prompt('Please enter a number between 0 and ' ${max}));
+let answer = yName.charAt(num);
+alert(`The character at your selected position is: ${answer}.`);
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
