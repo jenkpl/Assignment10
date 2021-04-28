fb986a47fcafd0a2c3e77b39262d24178b34befe
jenkpl <jenkpl@gmail.com>
2021-04-27 19:44:25 -0700

    Update deathbyjs.js

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -1,8 +1,25 @@
-//STEP 1:Write a JavaScript function that returns a passed string with letters in alphabetical order. Assume punctuation and number symbols are not included in the passed-in string.Sample Data and OutputExample string: 'webmaster' Expected Output: 'abeemrstw
+//$STEP 1:Write a JavaScript function that returns a passed string with letters in alphabetical order. Assume punctuation and number symbols are not included in the passed-in string.Sample Data and OutputExample string: 'webmaster' Expected Output: 'abeemrstw
+// let it = prompt('Enter a word or sentence without punctuation.');
+// function organizeIt() {
+// let lowerIt = it.toLowerCase();
+// let splitUp = lowerIt.split(''); //is a substring of the characters
+// let sorted  = splitUp.sort();//sorts the characters a to z
+// let result  = sorted.toString().replace(/,/g,'');//removes the commas
+// alert(`Your result is: ${result}`)
+// };
+// organizeIt();
 
-//STEP 2:Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string to upper case.Sample Data and OutputExample string: 'the quick brown fox' Expected Output: 'The Quick Brown Fox'
+//$STEP 2:Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string to upper case.Sample Data and OutputExample string: 'the quick brown fox' Expected Output: 'The Quick Brown Fox'
+// let it = prompt('Enter a word or sentence in lowercase characters.');
+// function capFirst() {
+//     let locate = it.charAt(0);
+//     let upper  = locate.toUpperCase();
+//     alert(upper);
+// }
+// capFirst();
 
 //STEP 3:Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string. Sample Data and Output Example string: 'The quick brown fox' Expected Output: 5
+
 
 
 //STEP 4:Write a JavaScript function that generates a string id (specified length) of random characters.Sample Data and OutputExpected Output: RCv1SBUC or PmJkc2Y2 or nIi5L0FA
